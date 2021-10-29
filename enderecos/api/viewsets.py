9375a7de2369f9738227a8d2f4a77e90ad9a16e6
wagner <wagherculano@hotmail.com>
2021-10-28 22:46:41 -0300

    Breve explicação sobre lookup_field

--- enderecos/api/viewsets.py
+++ enderecos/api/viewsets.py
@@ -9,4 +9,7 @@ class EnderecoViewSet(ModelViewSet):
     serializer_class = EnderecoSerializer
     filter_backends = (SearchFilter, )
     search_fields = ['endereco_1', 'cidade', 'estado', 'pais']
-
+    # lookup_field = 'nome'
+    # altera o item padrão de busca (pk). O campo escolhido deverá ser unico no banco para manter a integridade do BD.
+    # Pois não é possivel retornar mais de um dado. Assim, ao inves de usar o endpoint/pk/ pode-ser usar
+    # endpoint/item_definido_no_lookup/
