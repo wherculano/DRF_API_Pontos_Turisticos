19bd78f4c8ec74312b683021d18b99426d16f022
wagner <wagherculano@hotmail.com>
2021-10-29 22:35:57 -0300

    Implementado Nested Relantionships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from core.models import PontoTuristico
+from enderecos.api.serializers import EnderecoSerializer
+from atracoes.api.serializers import AtracaoSerializer
 
 
 class PontoTuristicoSerializer(serializers.ModelSerializer):
+    endereco = EnderecoSerializer()
+    atracoes = AtracaoSerializer(many=True)
+
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'nome', 'descricao', 'foto']  # , aprovado, comentarios, avaliacoes, endereco
+        fields = ['id', 'nome', 'descricao', 'endereco', 'atracoes', 'foto']
