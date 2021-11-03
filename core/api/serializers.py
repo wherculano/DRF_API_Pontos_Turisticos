c363b91366a9d9354066a224cec6ea793af7127f
wagner <wagherculano@hotmail.com>
2021-11-03 16:32:32 -0300

    Implementado ManyToMany RelationShip

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,19 +1,36 @@
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
-from core.models import PontoTuristico
-from enderecos.api.serializers import EnderecoSerializer
+
 from atracoes.api.serializers import AtracaoSerializer
+from core.models import PontoTuristico, Atracao
+from enderecos.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(serializers.ModelSerializer):
     endereco = EnderecoSerializer()
     atracoes = AtracaoSerializer(many=True)
-    descricao_completa = SerializerMethodField()
+    descricao_completa = SerializerMethodField(read_only=True)
 
     class Meta:
         model = PontoTuristico
         fields = ['id', 'nome', 'descricao', 'endereco', 'atracoes', 'foto', 'descricao_completa']
+        read_only_fields = ('comentarios', 'avaliacoes')
 
     @staticmethod
     def get_descricao_completa(obj):
         return f'{obj.nome}: {obj.descricao}'
+
+    # -- Criando Relação Many To Many --
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']
+        del validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.create_atracoes(atracoes, ponto)
+
+        return ponto
+
+    @staticmethod
+    def create_atracoes(atracoes, ponto):
+        for atracao in atracoes:
+            atrac = Atracao.objects.create(**atracao)
+            ponto.attrcoes.add(atrac)
