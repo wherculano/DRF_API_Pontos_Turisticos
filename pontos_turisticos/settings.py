d6b89cb8b18c8947cbea3f52d3d8da629fb49874
wagner <wagherculano@hotmail.com>
2021-11-03 22:42:45 -0300

    Adicionada paginacao

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -138,5 +138,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",)
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
 }
+
+
